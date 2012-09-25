c6b3404dae35154f2f4f0905cf4e2f1247edb07f
Matt Stine <matt.stine@gmail.com>
2012-09-24 23:14:58 -0500

    login is working

--- app.js
+++ app.js
@@ -2,8 +2,29 @@ load('vertx.js');
 
 var webServerConf = {  
 	port: 8080,
-	host: 'localhost'
+	host: 'localhost',
+	bridge: true,
+	inbound_permitted: [
+		{
+					address: 'vertx.basicauthmanager.login'
+	  },
+		{
+						address: 'vertx.mongopersistor'
+		}
+	]
 };
 
 vertx.deployModule('vertx.web-server-v1.0', webServerConf);
+
+var mongoConf = {
+				db_name: 'twitx'
+}
+
+vertx.deployModule('vertx.mongo-persistor-v1.0', mongoConf);
+
+var securityConf = {
+				user_collection: 'profiles'
+};
+
+vertx.deployModule('vertx.auth-mgr-v1.0', securityConf);
 
--- web/js/twitx.js
+++ web/js/twitx.js
@@ -2,6 +2,7 @@ $(function() {
 
 	function TwitxStreamViewModel() {
 					var self = this;
+					var eb = new vertx.EventBus(window.location.protocol + '//' + window.location.hostname + ':' + window.location.port + '/eventbus');
 
 					self.messages = ko.observableArray([
 						 {msg:"Hello from Twit.x Land!"},
@@ -14,6 +15,55 @@ $(function() {
 					self.twitxIt = function() {
 									self.messages.unshift({msg:self.twitxBox()});
 					}
+
+					self.username = ko.observable('');
+					self.password = ko.observable('');
+					self.sessionID = ko.observable('');
+
+					self.authenticated = ko.computed(function() {
+									return self.sessionID() != '';
+					});
+
+					self.showLoginForm = ko.computed(function() {
+									return !self.authenticated();
+					});
+
+					self.login = function() {
+						if (self.username().trim() != '' && self.password().trim() != '') {
+							eb.send('vertx.basicauthmanager.login', {username: self.username(), password: self.password()}, function (reply) {
+								if (reply.status === 'ok') {
+									self.sessionID(reply.sessionID);
+									self.fetchUserProfile(self.username());
+								} else {
+								alert(reply.status);				
+									alert('invalid login');
+								}
+							});
+						}
+					}
+
+				self.profile = ko.observable(new Profile());	
+
+					self.fetchUserProfile = function() {
+									if (self.username().trim() != '') {
+													eb.send('vertx.mongopersistor', {action:'findone', collection:'profiles', matcher: {username:self.username()}}, function(reply) {
+																	if (reply.status === 'ok') {
+																		self.profile(new Profile(reply.result));	
+																	}})}};
+																				
+
+		function Profile(json) {
+						if (json) {
+						var that = this;
+
+										that._id = json._id;
+										that.username = json.username;
+										that.password = json.password;
+										that.email = json.email;
+										that.fullName = json.fullname;
+						}
+		}
+
 	};	
 
 	ko.applyBindings(new TwitxStreamViewModel());
