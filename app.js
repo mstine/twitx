f2b5b32f65ec6b5f5581ed4ff9c546bb91d22e1b
Matt Stine <matt.stine@gmail.com>
2012-10-09 13:18:52 -0700

    first cut of workable demo

--- app.js
+++ app.js
@@ -1,30 +1,91 @@
 load('vertx.js');
 
+// Web Server Configuration
 var webServerConf = {  
 	port: 8080,
 	host: 'localhost',
 	bridge: true,
 	inbound_permitted: [
-		{
-					address: 'vertx.basicauthmanager.login'
-	  },
-		{
-						address: 'vertx.mongopersistor'
-		}
-	]
-};
+    { address: 'vertx.basicauthmanager.login' },
+    { address: 'vertx.mongopersistor' },
+    { address: 'messages.incoming' },
+    { address: 'messages.primestream' },
+  ],
+  outbound_permitted: [
+    { address_re: 'messages.outgoing\\..+' }
+]};
 
+// Deploy Web Server Module
 vertx.deployModule('vertx.web-server-v1.0', webServerConf);
 
+// MongoDB Persistor Configuration
 var mongoConf = {
-				db_name: 'twitx'
+  db_name: 'twitx'
 }
 
-vertx.deployModule('vertx.mongo-persistor-v1.0', mongoConf);
+// Deploy MongoDB Persistor Module
+vertx.deployModule('vertx.mongo-persistor-v1.1', mongoConf);
 
+// Authentication Manager Configuration
 var securityConf = {
-				user_collection: 'profiles'
+  user_collection: 'profiles'
 };
 
+// Deploy Authentication Manager Module
 vertx.deployModule('vertx.auth-mgr-v1.0', securityConf);
+
+var eb = vertx.eventBus;
+
+// Handle incoming messages from the browser...
+var messageHandler = function(message) {
+  message.timestamp = new Date();
+  eb.send('vertx.mongopersistor', { action: 'save',
+          collection: 'messages',
+          document: message });
+
+  eb.send('vertx.mongopersistor', { action : 'find',
+          collection: 'profiles',
+          matcher: { following: message.username }
+  },
+  function (reply) {
+    // This should really be on a worker...refactor!
+    for (var i = 0; i < reply.results.length; i++) {
+      // Stream the message to each person that's following me...
+      eb.send('messages.outgoing.'+reply.results[i].username, message);
+    }
+    // Send the message to me as well...
+    eb.send('messages.outgoing.'+message.username, message);
+  });
+}
+eb.registerHandler('messages.incoming', messageHandler);
+
+function loadUserProfile(username, callback) {
+  eb.send('vertx.mongopersistor', 
+          { action: 'findone',
+            collection: 'profiles',
+            matcher: { username: username } 
+          }, 
+          callback);
+}
+
+// Prime the message stream after loading user profiles...
+eb.registerHandler('messages.primestream', function(message, replier) {
+  loadUserProfile(message.username, function(reply) {
+    if (reply.status === 'ok') {
+      eb.send('vertx.mongopersistor', 
+              { action: 'find',
+                collection: 'messages',
+                matcher: { 
+                  username: { "$in": reply.result.following } 
+                },
+                sort: {timestamp: -1},
+                limit: 10 }, 
+                function(queryReply) {
+                  eb.send('messages.outgoing.'+reply.result.username, queryReply)
+                });
+                replier(reply);
+    }
+  });
+});
+
 
--- web/js/twitx.js
+++ web/js/twitx.js
@@ -4,16 +4,14 @@ $(function() {
 					var self = this;
 					var eb = new vertx.EventBus(window.location.protocol + '//' + window.location.hostname + ':' + window.location.port + '/eventbus');
 
-					self.messages = ko.observableArray([
-						 {msg:"Hello from Twit.x Land!"},
-						 {msg:"I'm not sure what I'm doing here..."},
-						 {msg:"Wheels-down SFO!"}
-					]);
+					self.messages = ko.observableArray();
 
-					self.twitxBox = ko.observable("Here's my message...");
+					self.twitxBox = ko.observable();
 
 					self.twitxIt = function() {
-									self.messages.unshift({msg:self.twitxBox()});
+            eb.send('messages.incoming', { username: self.profile().username,
+                    message:self.twitxBox() });
+            self.twitxBox('');  
 					}
 
 					self.username = ko.observable('');
@@ -29,42 +27,54 @@ $(function() {
 					});
 
 					self.login = function() {
-						if (self.username().trim() != '' && self.password().trim() != '') {
-							eb.send('vertx.basicauthmanager.login', {username: self.username(), password: self.password()}, function (reply) {
-								if (reply.status === 'ok') {
-									self.sessionID(reply.sessionID);
-									self.fetchUserProfile(self.username());
-								} else {
-								alert(reply.status);				
-									alert('invalid login');
-								}
-							});
-						}
-					}
+            if (self.username().trim() != '' && self.password().trim() != '') {
+              eb.send('vertx.basicauthmanager.login', {username: self.username(), password: self.password()}, function (reply) {
+                if (reply.status === 'ok') {
+                  self.sessionID(reply.sessionID);
+                  self.primeMessageStream(self.username());
+
+                  eb.registerHandler('messages.outgoing.'+self.username(), function(message) {
+                    // If this is a result of prime, will be a JSON object w/ results array
+                    if ($.isArray(message.results)) {
+                      self.messages.unshift.apply(self.messages, message.results);
+                    // Otherwise it is a plain message
+                    } else {
+                      self.messages.unshift(message);
+                    }
+                  });
+                } else {
+                  alert('Invalid Username/Password. Please try again.');
+                }
+              });
+            }
+          }
 
 				self.profile = ko.observable(new Profile());	
 
-					self.fetchUserProfile = function() {
-									if (self.username().trim() != '') {
-													eb.send('vertx.mongopersistor', {action:'findone', collection:'profiles', matcher: {username:self.username()}}, function(reply) {
-																	if (reply.status === 'ok') {
-																		self.profile(new Profile(reply.result));	
-																	}})}};
-																				
+        self.primeMessageStream = function(username) {
+          if (username.trim() != '') {
+            eb.send('messages.primestream', { username: username }, 
+                    function(reply) {
+                      if (reply.status === 'ok') {
+                        self.profile(new Profile(reply.result));	
+                      }
+                    });
+          } 
+        };
 
-		function Profile(json) {
-						if (json) {
-						var that = this;
+        function Profile(json) {
+          if (json) {
+            var that = this;
 
-										that._id = json._id;
-										that.username = json.username;
-										that.password = json.password;
-										that.email = json.email;
-										that.fullName = json.fullname;
-						}
-		}
+            that._id = json._id;
+            that.username = json.username;
+            that.password = json.password;
+            that.email = json.email;
+            that.fullName = json.fullname;
+          }
+        }
 
-	};	
+  };	
 
 	ko.applyBindings(new TwitxStreamViewModel());
 
